708851fc772e70636444a9307ad8cf5328b0b0c4
mariam2624 <58119277+mariam2624@users.noreply.github.com>
2021-08-03 23:59:58 +0500

    Adding

---
+++ JavaScript Practice/myscript2.js
@@ -0,0 +1,16 @@
+var ham = 7;
+var chesse = 10;
+
+var report = "Blank";
+
+if (ham >=10 && chesse >= 10)
+{
+  console.log("Strong sales of items");
+}
+else if (ham == 0 && chesse == 0)
+{
+  console.log("Nothing Sold");
+}
+else {
+  console.log("We had some sales of items");
+}
