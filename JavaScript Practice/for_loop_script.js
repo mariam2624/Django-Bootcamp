e681088eeca187f7425f6e12fc7f773c7f56874a
mariam2624 <58119277+mariam2624@users.noreply.github.com>
2021-08-12 21:41:08 +0500

    Adding

---
+++ JavaScript Practice/for_loop_script.js
@@ -0,0 +1,18 @@
+for (var num = 0; num < 5; num++) {
+  console.log("number is " +num);
+}
+
+for (var i = 0; i < 5; i++) {
+  console.log("Number is " +i);
+}
+
+var word = "abcdefgh"
+
+for (var i = 0; i < word.length; i++) {
+ console.log(word[i]);
+}
+
+var word_one = "abababab"
+for (var i = 0; i < word_one.length; i=i+2) {
+  console.log(word_one[i]);
+  }
